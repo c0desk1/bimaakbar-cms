84cbe745ba35ad71b7a167e77087fab30200eaac
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-08-14 19:03:24 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      }
+    ],
+    formats: ['image/webp', 'image/avif'],
 };
 
 export default nextConfig;
